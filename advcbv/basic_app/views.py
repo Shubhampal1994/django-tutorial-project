7ed94f810841453f88b6a69f13a1c8019c3a039c
VE_Shubham <shubhampaliwal@virtualemployee.com>
2018-09-27 17:37:06 +0530

    advcbv-complete

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.views.generic import (View, TemplateView, ListView, 
+								DetailView, CreateView, UpdateView,
+								DeleteView)
 from django.http import HttpResponse
 from basic_app import models
+from django.urls import reverse_lazy
 
 
 # class CBView(View):
@@ -9,22 +12,37 @@ from basic_app import models
 # 		return HttpResponse('CLASS BASED VIEWS!')
 
 
-# class IndexView(TemplateView):
-# 	template_name = 'index.html'
+class IndexView(TemplateView):
+	template_name = 'index.html'
 
-# 	def get_context_data(self, **kwargs):
-# 		context = super().get_context_data(**kwargs)
-# 		context['injectme'] = 'Basic Injections!'
-# 		return context
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['injectme'] = 'Basic Injections!'
+		return context
 
 
 class SchoolListView(ListView):
+	context_object_name = 'schools' # school_list by default
 	model = models.School
 
 
 class SchoolDetailView(DetailView):
+	context_object_name = 'school_detail' # school by default
 	model = models.School
-	template_name = 'basic_app/School_detail.html'
+	template_name = 'basic_app/school_detail.html'
+
+ 
+class SchoolCreateView(CreateView):
+	fields = ('name', 'principal', 'location')
+	model = models.School
 
 
-# Test Push VE
+class SchoolUpdateView(UpdateView):
+	fields = ('name', 'principal')
+	model = models.School
+
+
+class SchoolDeleteView(DeleteView):
+	model = models.School
+	success_url = reverse_lazy('basic_app:list')
+
